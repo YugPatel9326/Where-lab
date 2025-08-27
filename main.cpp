138f4947a6d6b836275bffc49ec323fb577820cd
YugPatel9326 <py41822@mail.harpercollege.edu>
2025-08-27 09:54:54 -0500

    Initial commit: Added C-string find functions

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int charFind(const char text[], char letter) {
+
+    int i = 0;
+    while (text[i] != '\0') {   
+        if (text[i] == letter) {
+            return i;  
+        }
+        i++;
+    }
+
+    return -1;  
+
+}
+
+int substringFind(const char str[], const char substr[]) {
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        int j = 0;
+        while (substr[j] != '\0' && str[i + j] == substr[j]) {
+            j++;
+        }
+        if (substr[j] == '\0') {
+            return i;
+        }
+    }
+
+    return -1;
+    
+}
+
+int main() {
+
+    const char sentence[] = "The quick brown fox";
+
+    cout << "Find Character 'a': " << charFind(sentence, 'a') << "\n";
+    cout << "Find Substring \"quick\": " << substringFind(sentence, "quick") << "\n";
+    cout << "Find Character ' ': " << charFind(sentence, ' ') << "\n";
+    cout << "Find Character 'z': " << charFind(sentence, 'z') << "\n";    
+    cout << "Find Substring \"quiet\": " << substringFind(sentence, "quiet") << "\n";
+    cout << "Find Substring \"fox\": " << substringFind(sentence, "fox") << "\n";
+
+    return 0;
+}
